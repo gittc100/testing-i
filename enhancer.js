1d2fc1a52e9378e30e0c88af824ce93f80f935bc
Thomas Claydon <tomclaydon102@gmail.com>
2019-01-28 18:52:09 -0500

    repair and success functions and testing completed

--- enhancer.js
+++ enhancer.js
@@ -17,53 +17,79 @@ function enhancementCheck(item) {
   if (next > 20) {
     throw new Error("Item Enhancement is at a maximum");
   } else {
-    item.enhancement += 1;
-    let name = item.originalName;
-    switch (next) {
-      case 1:
-      case 2:
-      case 3:
-      case 4:
-      case 5:
-      case 6:
-      case 7:
-      case 8:
-      case 9:
-      case 10:
-      case 11:
-      case 12:
-      case 13:
-      case 14:
-      case 15:
-        item.name = `[+${next}] ${name}`;
-        break;
-      case 16:
-        item.name = `[PRI] ${name}`;
-        break;
-      case 17:
-        item.name = `[DUO] ${name}`;
-        break;
-      case 18:
-        item.name = `[TRI] ${name}`;
-        break;
-      case 19:
-        item.name = `[TET] ${name}`;
-        break;
-      case 20:
-        item.name = `[PEN] ${name}`;
-        break;
+    if (
+      (item.durability < 20 && item.enhancement <= 14) ||
+      (item.durability < 0 && item.enhancement > 14 && item.enhancement <= 20)
+    ) {
+      throw new Error("Item Durability is too Low for Enhancement");
+    } else {
+      item.enhancement += 1;
+      let name = item.originalName;
+      switch (next) {
+        case 1:
+        case 2:
+        case 3:
+        case 4:
+        case 5:
+        case 6:
+        case 7:
+        case 8:
+        case 9:
+        case 10:
+        case 11:
+        case 12:
+        case 13:
+        case 14:
+        case 15:
+          item.name = `[+${next}] ${name}`;
+          break;
+        case 16:
+          item.name = `[PRI] ${name}`;
+          break;
+        case 17:
+          item.name = `[DUO] ${name}`;
+          break;
+        case 18:
+          item.name = `[TRI] ${name}`;
+          break;
+        case 19:
+          item.name = `[TET] ${name}`;
+          break;
+        case 20:
+          item.name = `[PEN] ${name}`;
+          break;
+      }
     }
   }
+}
+
+function checkEnhLevel(item) {
+  let type = item.type;
+  if (
+    (type === "armor" && item.enhancement <= 5) ||
+    (type === "weapon" && item.enhancement <= 7)
+  ) {
+    throw new Error(
+      `Failure is impossible Enhancement level too low for item Type: ${type}`
+    );
+  }
+}
+
+function failCheck(item) {
+  checkEnhLevel(item);
 }
 
 function success(item) {
   typeCheck(item);
   enhancementCheck(item);
-  console.log(item);
   return item;
 }
 
-function fail(item) {}
+function fail(item) {
+  typeCheck(item);
+  failCheck(item);
+  return item;
+}
 
 function repair(item) {
   typeCheck(item);
--- enhancer.spec.js
+++ enhancer.spec.js
@@ -1,6 +1,8 @@
 const en = require("./enhancer.js");
 
-describe("Repair Method", () => {
+//////////////////////
+
+describe("Repair Method: Success", () => {
   let itemToBeRepaired = {
     originalName: "Sword",
     name: "Sword",
@@ -17,43 +19,157 @@ describe("Repair Method", () => {
     enhancement: 0
   };
 
-  //   let itemToBeRepairedIncorrectType = {
-  //     originalName: "Sword",
-  //     name: "Sword",
-  //     type: "wrong",
-  //     durability: 90,
-  //     enhancement: 0
-  //   };
-
   test("repairs item", () => {
     expect(en.repair(itemToBeRepaired)).toEqual(itemRepaired);
   });
 });
 
-//   test("Incorrect Item Type", () => {
-//     expect(() => {
-//       en.repair(itemToBeRepairedIncorrectType);
-//     }).toThrow();
-//   });
+////////////////////////
 
-describe("Enhance Success Method", () => {
+describe("Repair Method Error: Type", () => {
+  let item = {
+    originalName: "Sword",
+    name: "Sword",
+    type: "not weapon",
+    durability: 90,
+    enhancement: 0
+  };
+
+  test("Type Incorrect", () => {
+    () => {
+        expect(en.repair(item)).toThrow();
+    };
+  });
+});
+
+/////////////////////////////
+
+describe("Enhance Method: Success", () => {
   let item = {
     originalName: "Sword",
     name: "Sword",
     type: "weapon",
     durability: 90,
-    enhancement: 1
+    enhancement: 15
   };
 
   let itemSuccess = {
     originalName: "Sword",
-    name: "[+2] Sword",
+    name: "[PRI] Sword",
     type: "weapon",
     durability: 90,
-    enhancement: 2
+    enhancement: 16
   };
 
-  test("enhance success", () => {
+  test("Enhance Success", () => {
     expect(en.success(item)).toEqual(itemSuccess);
   });
 });
+
+///////////////////////////////
+
+describe("Enhance Method Error: Type", () => {
+    let item = {
+      originalName: "Sword",
+      name: "Sword",
+      type: "not weapon",
+      durability: 90,
+      enhancement: 0
+    };
+  
+    test("Type Incorrect", () => {
+      () => {
+          expect(en.success(item)).toThrow();
+      };
+    });
+  });
+
+/////////////////////////////////
+
+describe("Enhance Method Error: Maxed Enhancement", () => {
+  let item = {
+    originalName: "Sword",
+    name: "Sword",
+    type: "weapon",
+    durability: 90,
+    enhancement: 20
+  };
+
+  it("Maxed Enhancement", () => {
+    () => {
+      expect(en.success(item)).toThrow();
+    };
+  });
+});
+
+/////////////////////////////////
+
+describe("Enhance Method Error: Durability Low 1", () => {
+    let item = {
+      originalName: "Sword",
+      name: "Sword",
+      type: "weapon",
+      durability: 18,
+      enhancement: 13
+    };
+  
+    it("Durability Minimized 1", () => {
+      () => {
+        expect(en.success(item)).toThrow();
+      };
+    });
+  });
+
+/////////////////////////////////
+
+describe("Enhance Method Error: Durability Low 2", () => {
+    let item = {
+      originalName: "Sword",
+      name: "Sword",
+      type: "weapon",
+      durability: -50,
+      enhancement: 15
+    };
+  
+    it("Durability Minimized 2", () => {
+      () => {
+        expect(en.success(item)).toThrow();
+      };
+    });
+  });
+
+////////////////////////
+
+describe("Fail Method Error: Type", () => {
+    let item = {
+      originalName: "Sword",
+      name: "Sword",
+      type: "not weapon",
+      durability: 90,
+      enhancement: 0
+    };
+  
+    test("Type Incorrect", () => {
+      () => {
+          expect(en.fail(item)).toThrow();
+      };
+    });
+  });
+
+////////////////////////////
+
+describe("Fail Method Error: Enhancement Level", () => {
+    let item = {
+      originalName: "Sword",
+      name: "Sword",
+      type: "weapon",
+      durability: 90,
+      enhancement: 5
+    };
+  
+    test("Enhancement Level is too Low", () => {
+      () => {
+          expect(en.fail(item)).toThrow();
+      };
+    });
+  });
